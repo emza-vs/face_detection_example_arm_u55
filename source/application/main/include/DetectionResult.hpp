24865ee0776f62381b9372bf8b1989a893b313f6
Michael Levit <michaell@emza-vs.com>
2022-01-16 13:14:16 +0200

    Added copyright 2022 and licence headers to all files

--- source/application/main/include/DetectionResult.hpp
+++ source/application/main/include/DetectionResult.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Arm Limited. All rights reserved.
+ * Copyright (c) 2022 Arm Limited. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
